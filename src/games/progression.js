19b0c4e37fcb2b3232d77c9fd7ab940a68a530f2
anton.potemkin <79133891911@yandex.ru>
2017-06-10 14:55:35 +0700

    fix progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,21 +2,20 @@ import { cons } from 'hexlet-pairs';
 import startGame from '../gameProcess';
 import getRandomNumber from '../randomGenerator';
 
-const condition = 'Answer "yes" if number even otherwise answer "no".';
+const condition = 'What number is missing in this progression?';
 const progressionLength = 10;
 
 const getPuzzle = () => {
   const firstElement = getRandomNumber(20);
   const step = getRandomNumber(5) + 1;
-  const progressionArray = [firstElement];
-  while (progressionArray.length <= progressionLength) {
-    progressionArray.push(firstElement + (step * progressionArray.length));
+  const progression = [firstElement];
+  while (progression.length <= progressionLength) {
+    progression.push(firstElement + (step * progression.length));
   }
   const indexOfAnswer = getRandomNumber(10);
-  const answer = progressionArray[indexOfAnswer];
-  progressionArray[indexOfAnswer] = '..';
-  const progression = progressionArray.reduce((acc, element) => `${acc}${element} `, '');
-  const question = `What number is missing in this progression?\n${progression}`;
+  const answer = progression[indexOfAnswer];
+  progression[indexOfAnswer] = '..';
+  const question = progression.reduce((acc, element) => `${acc}${element} `, '');
   return cons(question, `${answer}`);
 };
 
